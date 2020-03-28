a8b8b7b5842cadee72723f6a9d4142c5cb62869d
Sanjay <42981240+sanjays254@users.noreply.github.com>
2020-03-28 01:42:22 -0400

    removed extra code

--- app/controllers/slackathon/application_controller.rb
+++ app/controllers/slackathon/application_controller.rb
@@ -1,6 +1,6 @@
 module Slackathon
   class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    skip_before_action :verify_authenticity_token
+#     protect_from_forgery with: :exception
+#     skip_before_action :verify_authenticity_token
   end
 end
